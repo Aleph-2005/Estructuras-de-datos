bc87ca9d63f2ddefa8459329b8ae7e9ca9b39369
Elias <elopezr2300@alumno.ipn.mx>
2025-06-04 19:18:13 -0600

    lista

---
+++ c_escom/lista/listamain.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+int main()
+{
+    struct lista p;
+    init(&p);
+    for(int i=0;i<10;i++)
+    {
+        insertinicio(&p,i);
+        printf(" lista: %d\n",i);
+        imprimirlista(&p);
+    }
+    insertn(&p,34,1000);
+    printf("ahora\n");
+    imprimirlista(&p);
+    imprimirlista(&p);
+    printf("ahora_2\n");
+    printf("%d\n",eliminarPosicion(&p,3));
+    imprimirlista(&p);
+    return 0;
+}
